c33a8464537514793b7c13e33ec1174f37a0bcd7
holliropa <46962575+holliropa@users.noreply.github.com>
2025-06-28 16:36:49 +0200

    Add support multiple handlers per GLFW event

--- include/bw/glfw/event.h
+++ include/bw/glfw/event.h
@@ -5,17 +5,31 @@
 namespace glfw {
     template <typename... Args>
     class Event {
-        std::function<void(Args...)> _handler;
+        std::vector<std::pair<size_t, std::function<void(Args...)>>> _handlers;
+        size_t _next_id = 0;
 
     public:
         template <typename Callback>
-        void set(Callback&& callback_) {
-            _handler = std::forward<Callback>(callback_);
+        size_t set(Callback&& callback_) {
+            size_t id = _next_id++;
+            _handlers.emplace_back(id, std::forward<Callback>(callback_));
+            return id;
+        }
+
+        bool unregister(size_t handle) {
+            auto it = std::find_if(_handlers.begin(), _handlers.end(),
+                [handle](const auto& pair) { return pair.first == handle; });
+
+            if (it != _handlers.end()) {
+                _handlers.erase(it);
+                return true;
+            }
+            return false;
         }
 
         void operator()(Args... args_) {
-            if (_handler) {
-                _handler(args_...);
+            for (const auto& [id, handler] : _handlers) {
+                handler(args_...);
             }
         }
     };
